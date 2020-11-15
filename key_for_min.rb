a9efcc8fc9104a19d75599824ec5a02b3631f00f
Daniel Sasse <dsasse07@gmail.com>
2020-11-15 18:23:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,15 +4,16 @@
 def key_for_min_value(name_hash)
   min_value = name_hash.first[1]
   min_key = name_hash.first[0]
-  if name_hash
+ 
+  if !name_hash
+    return nil
+  else 
     name_hash.each do |key, value|
       if value < min_value
         min_value = value
         min_key = key
       end
     end
-  else
-    nil 
   end
   min_key
 end
